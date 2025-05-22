d1e90d481c106c714d2382665f24ad0db0718ff5
Vadim Bazhenov <bazhenov4job@gmail.com>
2025-05-22 20:32:59 +0300

    повторил за лектором действия по SQL

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostDao.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.repository
+
+import androidx.lifecycle.LiveData
+import ru.netology.nmedia.dto.Post
+
+interface PostDao {
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun shareById(id: Long)
+    fun removeById(id: Long)
+    fun save(post: Post)
+}
