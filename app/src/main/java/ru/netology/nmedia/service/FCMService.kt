6c810fd8340e05e77310ad056d1831218547fb53
Vadim Bazhenov <bazhenov4job@gmail.com>
2025-06-22 19:12:17 +0300

    добился получения уведомлений от сендера и оттображения в трее

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -1,15 +1,83 @@
 package ru.netology.nmedia.service
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
+import ru.netology.nmedia.R
+import kotlin.random.Random
 
 class FCMService: FirebaseMessagingService() {
+
+    private val gson = Gson()
+    private val content = "content"
+
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+
     override fun onNewToken(token: String) {
         println(token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-        println(Gson().toJson(message))
+        message.data["action"]?.let {
+            when (Action.valueOf(it)) {
+                Action.LIKE -> handleLike(gson.fromJson(message.data[content], Like::class.java))
+            }
+        }
     }
+
+    private fun handleLike(content: Like) {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(getString(R.string.notification_user_liked, content.userName, content.postAuthor))
+                .build()
+            NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "notifications"
+    }
+
 }
+
+enum class Action {
+    LIKE,
+}
+
+data class Like (
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String
+)
